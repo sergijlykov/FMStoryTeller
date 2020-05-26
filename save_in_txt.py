04f79fc4043b54e81529cb0cdee2f929c6e8df50
jibi <jibilearnpython@gmail.com>
2020-05-26 15:16:33 +0300

    multi line txt updater working version

--- save_in_txt.py
+++ save_in_txt.py
@@ -1,33 +1,22 @@
-import tkinter
 from tkinter import *
-from PIL import ImageTk,Image
 from tkinter.scrolledtext import ScrolledText
+from PIL import ImageTk,Image
 
-#writing entry to txt file named monte.txt
-def writeFile():
-    file = open('monte.txt', 'a+')
-    file.write(metinF.get() + '\n' + '\n')
-    file.close()
-    metinF.delete(0, END)
-
-#gui preferences
 gui = Tk()
 gui.title('FM Story Teller')
 gui.iconbitmap('C:/Users/Sergo/PycharmProjects/FMStoryTeller/img/Icon_Books.ico')
-gui.geometry('325x150')
 
-# Label(gui, text="ScrolledText Box:").grid(row=1, column=0)
-# st = ScrolledText(gui, height=5); st.grid(row=1, column=1)
-# Button(gui, text="Print Text", command=(lambda: writeFile)).grid(row=1, column=2, sticky="EW")
+def writeFile():
+    file = open('Story.txt', 'a+')
+    file.write(scrtxt.get("1.0", END) + '\n' + '\n')
+    file.close()
+    scrtxt.delete("1.0", END)
 
-#entry field size and position
-metinF = Entry(gui, width = 50)
-metinF.grid(row=9, column=1, padx = 10, pady = 10)
+scrtxt = ScrolledText(gui, height=5, padx = 10, pady = 10); scrtxt.grid(row=1, column=1)
 
-#button to save entry text in monte.txt
+#button to save text.txt
 buttonWrite = Button(gui)
-buttonWrite.config(text='Сохранить в файл',padx = 10, pady = 10, command = writeFile)
+buttonWrite.config(text='Save text',padx = 10, pady = 10, command = writeFile)
 buttonWrite.grid(row=10, column=1)
 
 gui.mainloop()
-
