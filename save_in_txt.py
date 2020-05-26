f1e57fcfbd69d2cc5f6cfb03f75f31b847aa72e4
jibi <jibilearnpython@gmail.com>
2020-05-26 17:42:37 +0300

    added some colors

--- save_in_txt.py
+++ save_in_txt.py
@@ -5,6 +5,7 @@ from PIL import ImageTk,Image
 gui = Tk()
 gui.title('FM Story Teller')
 gui.iconbitmap('C:/Users/Sergo/PycharmProjects/FMStoryTeller/img/Icon_Books.ico')
+gui.configure(bg='#607D8B')
 
 def writeFile():
     file = open('Story.txt', 'a+')
@@ -12,10 +13,10 @@ def writeFile():
     file.close()
     scrtxt.delete("1.0", END)
 
-scrtxt = ScrolledText(gui, height=5, padx = 10, pady = 10); scrtxt.grid(row=1, column=1)
+scrtxt = ScrolledText(gui, height=5, padx = 10, pady = 10, bg='#BDBDBD'); scrtxt.grid(row=1, column=1)
 
 #button to save text.txt
-buttonWrite = Button(gui)
+buttonWrite = Button(gui, bg='#455A64', fg ='#FFFFFF', font="roboto 12")
 buttonWrite.config(text='Save text',padx = 10, pady = 10, command = writeFile)
 buttonWrite.grid(row=10, column=1)
 
